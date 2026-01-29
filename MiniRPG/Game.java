34553c9908653fafed4d165660abf4a31d3fe07f
a253511sv <a253511sv@h21.informatica.escoladeltreball.org>
2026-01-29 11:35:33 +0100

    Experiment 1

--- MiniRPG/Game.java
+++ MiniRPG/Game.java
@@ -1,16 +1,6 @@
 public class Game {
-
-    public static void main(String[] args) {
-        String nom = "Sebas";
-        int vida = 100;
-        int nivell = 5;
-
-        mostrarJugador(nom, vida, nivell);
-    }
-
-    public static void mostrarJugador(String nom, int vida, int nivell) {
-        System.out.println("Nom: " + nom);
-        System.out.println("Vida: " + vida);
-        System.out.println("Nivell: " + nivell);
-    }
+	
+	public static void main (String[] args){
+		System.out.println("Experiment 1");
+	}
 }
