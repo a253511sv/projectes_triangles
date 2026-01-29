2017813540fd51f1961c0831dd786e91aadf9241
a253511sv <a253511sv@h21.informatica.escoladeltreball.org>
2026-01-29 10:10:48 +0100

    Afegida funció mostrarJugador amb variables

--- MiniRPG/Game.java
+++ MiniRPG/Game.java
@@ -1,5 +1,16 @@
 public class Game {
+
     public static void main(String[] args) {
-        System.out.println("Benvingut al Mini RPG!");
+        String nom = "Sebas";
+        int vida = 100;
+        int nivell = 5;
+
+        mostrarJugador(nom, vida, nivell);
+    }
+
+    public static void mostrarJugador(String nom, int vida, int nivell) {
+        System.out.println("Nom: " + nom);
+        System.out.println("Vida: " + vida);
+        System.out.println("Nivell: " + nivell);
     }
 }
